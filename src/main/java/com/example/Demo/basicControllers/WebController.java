c206df09fe441006ff73ead6d54d69aeb6b12739
Shanmugasundaram <sshanmugasu6@dxc.com>
2020-02-19 15:00:49 +0530

    web page for add added

--- src/main/java/com/example/Demo/basicControllers/WebController.java
+++ src/main/java/com/example/Demo/basicControllers/WebController.java
@@ -1,7 +1,10 @@
 package com.example.Demo.basicControllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebController {
@@ -17,5 +20,25 @@ public class WebController {
 	{
 		return "Unexpected Error occurred. Check the logs for details.";
 	}
+	
+	@PostMapping("getsum")
+	public String doSum(@RequestParam("num01") int num01, @RequestParam("num02") int num02, ModelMap modelMap)
+	{
+		int sum = num01 + num02;
+		System.out.println(num01);
+		System.out.println(num02);
+		System.out.println(sum);
+		modelMap.put("num01", num01);
+		modelMap.put("num02", num02);
+		modelMap.put("sum", sum);
+		return "result";
+	}
+	
+	@GetMapping("doadd")
+	public String doSum()
+	{
+		return "sum";
+	}
+
 
 }
---
+++ src/main/webapp/WEB-INF/view/jsp/sum.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Subbu's SB Demo App - Sum</title>
+</head>
+<body>
+<font color="#186A3B"><h1>Welcome to Spring Boot Demo Application - Sum</h1></font>
+<form action="getsum" method="post">
+  <label for="Num01">Number 01:</label>
+  <input type="number" id="num01" name="num01"><br><br>
+  <label for="Num02">Number 02:</label>
+  <input type="number" id="num02" name="num02"><br><br>
+  <input type="submit" value="Add">
+</form></body>
+</html>
